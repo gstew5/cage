347ac8c011d63154b73ce7f603a9e6921153cedc
Gordon Stewart <gstew5@gmail.com>
2016-10-03 13:27:28 -0400

    Ordered instance for resource

--- orderedtypes.v
+++ orderedtypes.v
@@ -5,7 +5,7 @@ From mathcomp Require Import all_algebra.
 Require Import Coq.FSets.FMapFacts.
 Require Import Structures.Orders NArith.
 
-Require Import compile.
+Require Import compile combinators.
 
 Module Type OrderedFinType.
    Parameter t : finType.
@@ -38,3 +38,56 @@ Module OrderedType_of_OrderedFinType (A : OrderedFinType)
       Definition compare := A.compare.
       Definition eq_dec := A.eq_dec.
 End OrderedType_of_OrderedFinType.                                
+
+Module OrderedResource <: OrderedFinType.
+  Definition t := [finType of resource]. 
+  Definition CTypeInstance := resourceCTypeInstance.
+  Definition RefineTypeAxiomInstance := resourceRefineTypeAxiomInstance.
+  Definition RefineTypeInstance := resourceRefineTypeInstance.
+  Definition eq r1 r2 := resource_eq r1 r2 = true.
+  Definition lt r1 r2 :=
+    match r1, r2 with
+    | RNo, RNo => False
+    | RNo, RYes => True
+    | RYes, RNo => False
+    | RYes, RYes => False
+    end.
+
+  Lemma eq_refl : forall x, eq x x.
+  Proof. by move => x; rewrite /eq; case: (@resource_eqP x x). Qed.
+
+  Lemma eq_sym : forall x y, eq x y -> eq y x.
+  Proof.
+    move => x y; rewrite /eq.
+    case: (@resource_eqP x y) => // -> _; apply: eq_refl.
+  Qed.
+
+  Lemma eq_trans : forall x y z, eq x y -> eq y z -> eq x z.
+  Proof. by move => x y z; rewrite /eq; case: (@resource_eqP x y) => // ->. Qed.
+
+  Lemma lt_trans : forall x y z, lt x y -> lt y z -> lt x z.
+  Proof. by move => x y z; case: x => //; case: y => //; case: z. Qed.
+
+  Lemma lt_not_eq : forall x y, lt x y -> ~eq x y.
+  Proof. by move => x y; case: x => //; case: y. Qed.
+
+  Lemma compare : forall x y, Compare lt eq x y.
+  Proof.
+    case => //; case => //.
+    { apply: EQ => //. }
+    { apply: GT => //. }
+    { apply: LT => //. }
+    apply: EQ => //.
+  Qed.
+
+  Lemma eq_dec : forall x y, {eq x y} + {~eq x y}.
+  Proof.
+    move => x y; rewrite /eq; case: (@resource_eqP x y).
+    { by move => _; left. }
+      by move => _; right.
+  Qed.
+
+  Lemma eqP : forall x y, x = y <-> eq x y.
+  Proof. by move => x y; rewrite /eq; case: (@resource_eqP x y). Qed.
+End OrderedResource.
+
