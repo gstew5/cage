25c92aade53b455f9ec5e01cfb7ae595807ad017
Gordon Stewart <gstew5@gmail.com>
2016-11-21 14:37:42 -0500

    Further work in dyadic.v

--- dyadic.v
+++ dyadic.v
@@ -7,12 +7,20 @@ Record D : Set :=
 Definition pow_pos (p r : positive) : positive :=
   Pos.iter (Pos.mul p) 1%positive r.
 
+Lemma pow_pos_Zpow_pos p r : Zpos (pow_pos p r) = Z.pow_pos (Zpos p) r.
+Proof.
+  unfold pow_pos, Z.pow_pos.
+  rewrite !Pos2Nat.inj_iter; generalize (Pos.to_nat r) as n; intro.
+  revert p; induction n; auto.
+  intros p; simpl; rewrite <-IHn; auto.
+Qed.
+
 Lemma pow_pos_plus (p r s : positive) :
   pow_pos p (r + s) = (pow_pos p r * pow_pos p s)%positive.
 Admitted.
 
 Definition D_to_Q (d : D) :=
-  Qmake (num d) (pow_pos 2 (den d)).
+  Qmake (num d) (shift_pos (den d) 1).
 
 Definition Dadd (d1 d2 : D) : D :=
   match d1, d2 with
@@ -20,35 +28,120 @@ Definition Dadd (d1 d2 : D) : D :=
     if Pos.ltb y1 y2 then
       Dmake (Z.pow_pos 2 (y2 - y1) * x1 + x2) y2
     else 
-      Dmake (Z.pow_pos 2 (y1 - y2) * x1 + x2) y1
+      Dmake (Z.pow_pos 2 (y1 - y2) * x2 + x1) y1
   end.
 
 Lemma Zpower_nat_is_exp' n m z :
   Zpower_nat z (n - m) = (Zpower_nat z n / Zpower_nat z m)%Z.
 Admitted.                           
 
+Lemma Qdiv_mult (s q r : Q) : (q / r) == (q * s) / (r * s).
+Proof.
+  unfold Qdiv.
+  assert (q * s * /(r * s) = q * /r) as ->.
+  { admit. }
+  apply Qeq_refl.
+Admitted.    
+
 Lemma Dadd_ok d1 d2 :
-  D_to_Q (Dadd d1 d2) = D_to_Q d1 + D_to_Q d2.
+  D_to_Q (Dadd d1 d2) == D_to_Q d1 + D_to_Q d2.
 Proof.
   destruct d1, d2; simpl.
   generalize den0 as X; intro.
   generalize num0 as Y; intro.
   generalize den1 as Z; intro.
   generalize num1 as W; intro.
+  unfold Qplus; simpl.
+  rewrite !shift_pos_correct, Qmake_Qdiv, !Pos2Z.inj_mul, !shift_pos_correct.
+  rewrite !Zmult_1_r, !inject_Z_plus, !inject_Z_mult.
+  assert (inject_Z (Z.pow_pos 2 X) * inject_Z (Z.pow_pos 2 Z) =
+          inject_Z (Z.pow_pos 2 (X + Z))) as ->.
+  { rewrite <-inject_Z_mult.
+    symmetry; rewrite !Zpower_pos_nat.
+    rewrite Pos2Nat.inj_add, Zpower_nat_is_exp; auto. }
   destruct (Pos.ltb X Z) eqn:H.
-  { unfold D_to_Q; simpl.
-    rewrite Zpower_pos_nat.
-    rewrite nat_of_P_minus_morphism.
-    { rewrite Zpower_nat_is_exp'.
-      unfold pow_pos.
-      
-      admit. }
-    apply nat_of_P_gt_Gt_compare_complement_morphism.
-    rewrite Pos.ltb_lt in H.
-    rewrite Pos2Nat.inj_lt in H.
+  { rewrite (Qdiv_mult (1 / inject_Z (Z.pow_pos 2 X))).
+    assert (((inject_Z Y * inject_Z (Z.pow_pos 2 Z) +
+              inject_Z W * inject_Z (Z.pow_pos 2 X)) *
+             (1 / inject_Z (Z.pow_pos 2 X)) ==
+             inject_Z Y * inject_Z (Z.pow_pos 2 (Z - X)) + inject_Z W)) as ->.
+    { unfold Qdiv; rewrite Qmult_1_l.
+      rewrite Qmult_plus_distr_l.
+      unfold inject_Z.
+      rewrite <-Qmult_assoc.
+      assert ((Z.pow_pos 2 Z # 1) * / (Z.pow_pos 2 X # 1) ==
+              Z.pow_pos 2 (Z - X) # 1) as ->.
+      { admit. }        
+      apply Qplus_inj_l.
+      rewrite <-Qmult_assoc, Qmult_inv_r.
+      { rewrite Qmult_1_r; apply Qeq_refl. }
+      rewrite Zpower_pos_nat, Zpower_nat_Z.
+      unfold Qeq; simpl; rewrite Zmult_1_r; apply Z.pow_nonzero.
+      { omega. }
+      omega. }
+    assert (inject_Z (Z.pow_pos 2 (X + Z)) * (1 / inject_Z (Z.pow_pos 2 X)) ==
+            inject_Z (Z.pow_pos 2 Z)) as ->.
+    { unfold Qdiv.
+      rewrite Qmult_assoc, Qmult_comm, Qmult_assoc.
+      rewrite (Qmult_comm (/_)).
+      assert (inject_Z (Z.pow_pos 2 (X + Z)) * / inject_Z (Z.pow_pos 2 X) ==
+              inject_Z (Z.pow_pos 2 Z)) as ->.
+      { rewrite Zpower_pos_is_exp, inject_Z_mult.
+        rewrite (Qmult_comm (inject_Z (Z.pow_pos 2 X))).
+        rewrite <-Qmult_assoc, Qmult_inv_r.
+        { rewrite Qmult_1_r; apply Qeq_refl. }
+        unfold inject_Z; rewrite Zpower_pos_nat, Zpower_nat_Z.
+        unfold Qeq; simpl; rewrite Zmult_1_r; apply Z.pow_nonzero.
+        { omega. }
+        omega. }
+      rewrite Qmult_1_r; apply Qeq_refl. }
+    unfold D_to_Q; simpl.
+    rewrite <-inject_Z_mult, <-inject_Z_plus.
+    assert (Z.pow_pos 2 Z = Z.pow_pos 2 Z * ' 1)%Z as ->.
+    { rewrite Zmult_1_r; auto. }
+    rewrite <-shift_pos_correct, <-Qmake_Qdiv.
+    rewrite Zmult_comm; apply Qeq_refl; auto. }
+  rewrite (Qdiv_mult (1 / inject_Z (Z.pow_pos 2 Z))).
+  assert (((inject_Z Y * inject_Z (Z.pow_pos 2 Z) +
+            inject_Z W * inject_Z (Z.pow_pos 2 X)) *
+           (1 / inject_Z (Z.pow_pos 2 Z)) ==
+           inject_Z Y + inject_Z W * inject_Z (Z.pow_pos 2 (X - Z)))) as ->.
+  { unfold Qdiv; rewrite Qmult_1_l.
+    rewrite Qmult_plus_distr_l.
+    unfold inject_Z.
+    rewrite <-(Qmult_assoc (W # 1)).
+    assert ((Z.pow_pos 2 X # 1) * / (Z.pow_pos 2 Z # 1) ==
+            Z.pow_pos 2 (X - Z) # 1) as ->.
+    { admit. }
+    apply Qplus_inj_r.
+    rewrite <-Qmult_assoc, Qmult_inv_r.
+    { rewrite Qmult_1_r; apply Qeq_refl. }
+    rewrite Zpower_pos_nat, Zpower_nat_Z.
+    unfold Qeq; simpl; rewrite Zmult_1_r; apply Z.pow_nonzero.
+    { omega. }
     omega. }
-  admit.
-Admitted.    
+  assert (inject_Z (Z.pow_pos 2 (X + Z)) * (1 / inject_Z (Z.pow_pos 2 Z)) ==
+          inject_Z (Z.pow_pos 2 X)) as ->.
+    { unfold Qdiv.
+      rewrite Qmult_assoc, Qmult_comm, Qmult_assoc.
+      rewrite (Qmult_comm (/_)).
+      assert (inject_Z (Z.pow_pos 2 (X + Z)) * / inject_Z (Z.pow_pos 2 Z) ==
+              inject_Z (Z.pow_pos 2 X)) as ->.
+      { rewrite Zpower_pos_is_exp, inject_Z_mult.
+        rewrite <-Qmult_assoc, Qmult_inv_r.
+        { rewrite Qmult_1_r; apply Qeq_refl. }
+        unfold inject_Z; rewrite Zpower_pos_nat, Zpower_nat_Z.
+        unfold Qeq; simpl; rewrite Zmult_1_r; apply Z.pow_nonzero.
+        { omega. }
+        omega. }
+      rewrite Qmult_1_r; apply Qeq_refl. }
+  unfold D_to_Q; simpl.
+  rewrite <-inject_Z_mult, <-inject_Z_plus.
+  assert (Z.pow_pos 2 X = Z.pow_pos 2 X * ' 1)%Z as ->.
+  { rewrite Zmult_1_r; auto. }
+  rewrite <-shift_pos_correct, <-Qmake_Qdiv.
+  rewrite Zmult_comm, Z.add_comm; apply Qeq_refl.
+Admitted.
 
 Definition Dmult (d1 d2 : D) : D :=
   match d1, d2 with
@@ -56,6 +149,13 @@ Definition Dmult (d1 d2 : D) : D :=
     Dmake (x1 * x2) (y1 + y2)
   end.
 
+Lemma shift_nat1_mult n m :
+  (shift_nat n 1 * shift_nat m 1 = shift_nat n (shift_nat m 1))%positive.
+Proof.
+  induction n; simpl; auto.
+  rewrite IHn; auto.
+Qed.
+ 
 Lemma Dmult_ok d1 d2 :
   D_to_Q (Dmult d1 d2) = D_to_Q d1 * D_to_Q d2.
 Proof.
@@ -66,5 +166,6 @@ Proof.
   generalize num1 as W; intro.
   unfold D_to_Q; simpl.
   unfold Qmult; simpl.
-  rewrite pow_pos_plus; auto.
+  rewrite !shift_pos_nat, Pos2Nat.inj_add, shift_nat_plus.
+  rewrite shift_nat1_mult; auto.
 Qed.
