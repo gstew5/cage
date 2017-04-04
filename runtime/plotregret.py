Fix confidence interval calculation

runtime/plotregret.py
@@ -103,15 +103,15 @@ ax.get_xaxis().tick_bottom()
 ax.get_yaxis().tick_left()
 ## END Randal Olson stuff
 
-plt.title('Routing', size=32)
+plt.title('Load Balancing', size=32)
 plt.yscale('symlog',linthreshy=0.01) #so we properly support negative values (error bars)
 plt.minorticks_off()
 plt.xlim([-1,regret_mean.keys()[len(regret_mean.keys())-1]+1])
 plt.plot(range(len(bound_mean)), bound_mean.values(), '--', color=tableau20[0], linewidth=4)
 plt.plot(range(len(cost_mean)), cost_mean.values(), '-', color=tableau20[1], linewidth=4)
 
-degrees_freedom = np.full(len(regret_std), samplesize)
-errorbars = ss.t.ppf(confidencelevel, degrees_freedom)*regret_std.values()
+degrees_freedom = np.full(len(regret_std), samplesize-1)
+errorbars = ss.t.ppf((1+confidencelevel)/2., degrees_freedom)*regret_std.values()
 
 (_, caps, _) = plt.errorbar(
     range(len(regret_mean)),