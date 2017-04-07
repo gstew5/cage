33929301338faae7afe56c3676de198992368edd
Gordon Stewart <gstew5@gmail.com>
2017-04-07 16:54:39 -0400

    Remove neps_exp_le.v

--- mwucount.sh
+++ mwucount.sh
@@ -3,7 +3,7 @@
 # Use coqwc to count the number of lines of proof and spec in
 # files related to the implementation of MWU.
 
-MWUFILES="weights.v weightsextract.v weightslang.v dyadic.v neps_exp_le.v compile.v dist.v numerics.v orderedtypes.v extrema.v bigops.v strings.v"
+MWUFILES="weights.v weightsextract.v weightslang.v dyadic.v compile.v dist.v numerics.v orderedtypes.v extrema.v bigops.v strings.v"
 
 eval "coqwc "$MWUFILES""
 
