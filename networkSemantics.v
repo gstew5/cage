1de85aaad03f0bd729f2531ccfc135347a67befa
Alexander Bagnall <ab667712@ohio.edu>
2017-08-09 19:42:37 -0400

    First pass at network operational semantics. Renamed networkStep to network_step just to match machine_step.

--- networkSemantics.v
+++ networkSemantics.v
@@ -18,15 +18,38 @@ Section NetworkSemantics.
       initialized : bool}.
 
   Variable network_desc : node -> pkg.
+
+  Definition localStateTy := forall (n : node), state (network_desc n).
   
   Record world :=
     mkWorld {
-      localState : forall (n : node), state (network_desc n);
+      localState : localStateTy ;
       inFlight : list packet ;
       trace : list event}.
 
-  Inductive networkStep : world -> world -> Prop :=
-  | initStep : forall (w1 w2 : world), networkStep w1 w2
-  | packetStep : forall (w1 w2 : world), networkStep w1 w2.
+  Definition upd_localState (n : node) (s : state (network_desc n)) (ls : localStateTy)
+    : localStateTy :=
+    fun n' => ls n' . (* TODO *)
+
+  Open Scope list_scope.
+
+  Inductive network_step : world -> world -> Prop :=
+  | initStep : forall (w1 w2 : world) (n : node)
+                 (st : state (network_desc n)) (ps : list packet)
+                 (es : list event),
+      (network_desc n).(initialized) = false ->
+      (network_desc n).(init) tt = (st, ps, es) ->
+      network_step w1 (mkWorld (upd_localState n st w1.(localState))
+                              (ps ++ w1.(inFlight)) (es ++ w1.(trace)))
+
+  | packetStep : forall (w1 w2 : world) (n : node) (p : packet)
+                   (l1 l2 : list packet) (st : state (network_desc n))
+                   (ps : list packet) (es : list event),
+      (network_desc n).(initialized) = true ->
+      w1.(inFlight) = l1 ++ (p :: l2) ->
+      fst p = n ->
+      (network_desc n).(recv) (snd p) (w1.(localState) n) = (st, ps, es) ->
+      network_step w1 (mkWorld (upd_localState n st w1.(localState))
+                              (ps ++ w1.(inFlight)) (es ++ w1.(trace))).
 
 End NetworkSemantics.
