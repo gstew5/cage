e323e29c7d1c07bf8cb34cd7d795c0bc654ac804
Gordon Stewart <gstew5@gmail.com>
2017-09-14 11:10:03 -0400

    Prove that the constant classifier has VC dimension 0 - The constant classifier maps all inputs to a particular label, l1 - Assumptions:   + The label space contains at least 2 distinct labels   + The domain being classified is inhabited

--- vc.v
+++ vc.v
@@ -7,6 +7,8 @@ Require Import mathcomp.ssreflect.ssreflect.
 From mathcomp Require Import all_ssreflect.
 From mathcomp Require Import all_algebra.
 
+Definition Pred (T : Type) := T -> Prop.
+
 Section VC_dimension.
   Variable T : Type. (*input domain*)
   Variable L : finType. (*labels*)
@@ -14,10 +16,10 @@ Section VC_dimension.
   Definition T_set (n : nat) := 'I_n -> T. (*sets of T's of size n*)
   Definition labeling := T -> L.
   (*a hypothesis space is a subset of the space of functions from T to L*)
-  Definition hypothesis_space (H : pred labeling) := {h : labeling | H h}.
+  Definition hypothesis_space (H : Pred labeling) := {h : labeling | H h}.
 
   Section shatters.
-    Variable H : pred labeling.
+    Variable H : Pred labeling.
 
     (*a hypothesis space shatters a natural number n if for all T-sets
       s of size n and for any labeling f, there's a hypothesis in the
@@ -26,11 +28,35 @@ Section VC_dimension.
       forall s : T_set n, 
       forall f : labeling,
       exists h : hypothesis_space H,
-      forall i : 'I_n, val h (s i) = f (s i).
+      forall i : 'I_n, projT1 h (s i) = f (s i).
   End shatters.
 
   (*the VC dimension of a hypothesis space is the largest n it shatters*)
-  Definition VC_dim (H : pred labeling) (n : nat) : Prop :=
+  Definition VC_dim (H : Pred labeling) (n : nat) : Prop :=
     shatters H n /\ ~shatters H n.+1.
 End VC_dimension.  
 
+(*examples*)
+Section constant_classifier.
+  Variable T : Type.
+  Variable t : T.
+  Variable L : finType.
+  Variable l1 : L.
+  Variable l2 : L.
+  Variable l1_neq_l2 : l1 <> l2.
+
+  Definition constant_classifier : Pred (labeling T L) :=
+    fun f : labeling T L => forall t, f t = l1.
+
+  Lemma l1_cc : constant_classifier (fun _ => l1).
+  Proof. by []. Qed.
+
+  Definition T_set_t : T_set T 1 := fun _ => t.
+  
+  Theorem cc_vc0 : VC_dim constant_classifier 0.
+  Proof.
+    split; first by move => s f; exists (exist _ _ l1_cc); case.
+    move/(_ T_set_t (fun _ => l2)); case => x; move/(_ ord0).
+    by case: x => /= x ->.
+  Qed.
+End constant_classifier.  
